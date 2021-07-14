50dddbcddfbab3b35ffd2820b48b4316245d5279
caojoshua <cao.joshua@yahoo.com>
2021-07-13 23:34:31 -0700

    Code gen parenthesis if needed. toString functions for debugging.

--- include/Node.h
+++ include/Node.h
@@ -7,6 +7,7 @@
 class Node {
   public:
     virtual std::string codeGen() = 0;
+    virtual std::string toString() = 0;
 };
 
 class BinaryOpNode : public Node {
@@ -20,20 +21,28 @@ class BinaryOpNode : public Node {
 
     BinaryOpNode(Op op, Node *left, Node *right) : op(op), left(left), right(right) {};
     std::string codeGen();
+    std::string toString();
     Op getOp();
     Node *getLeft();
     Node *getRight();
+    bool higherPrecedence(BinaryOpNode *other);
+    bool samePrecedence(BinaryOpNode *other);
+    unsigned getOpPrecedence();
+    void unknownOperatorError();
 
   private:
     Op op;
     Node *left;
     Node *right;
+
+    std::string opToStr();
 };
 
 class IdentifierNode : public Node {
   public:
     IdentifierNode(std::string identifier) : identifier(identifier) {}
     std::string codeGen();
+    std::string toString();
     std::string getIdentifier();
 
   private:
@@ -44,6 +53,7 @@ class NumberNode : public Node {
   public:
     NumberNode(float number) : number(number) {}
     std::string codeGen();
+    std::string toString();
     float getNumber();
 
   private:
--- src/Node.cpp
+++ src/Node.cpp
@@ -22,7 +22,26 @@ std::string BinaryOpNode::codeGen() {
       exit(1);
   }
 
-  return left->codeGen() + ' ' + op + ' ' + right->codeGen();
+  std::string leftString = left->codeGen();
+  std::string rightString = right->codeGen();
+
+  BinaryOpNode *leftBinaryOpNode = dynamic_cast<BinaryOpNode *>(left);
+  BinaryOpNode *rightBinaryOpNode = dynamic_cast<BinaryOpNode *>(right);
+
+  if (leftBinaryOpNode && this->getOpPrecedence() > leftBinaryOpNode->getOpPrecedence()) {
+    leftString = '(' + leftString + ')';
+  }
+
+  if (rightBinaryOpNode && this->getOpPrecedence() > rightBinaryOpNode->getOpPrecedence()) {
+    rightString = '(' + rightString + ')';
+  }
+
+  return leftString + ' ' + op + ' ' + rightString;
+}
+
+std::string BinaryOpNode::toString() {
+  // TODO: concisely print left and right nodes
+  return opToStr();
 }
 
 BinaryOpNode::Op BinaryOpNode::getOp() {
@@ -37,7 +56,58 @@ Node * BinaryOpNode::getRight() {
   return this->right;
 }
 
+std::string BinaryOpNode::opToStr() {
+  switch (this->op) {
+    case Add:
+      return "+";
+      break;
+    case Sub:
+      return "-";
+      break;
+    case Mul:
+      return "*";
+      break;
+    case Div:
+      return "/";
+      break;
+    default:
+      unknownOperatorError();
+  }
+  return "";
+}
+
+bool BinaryOpNode::higherPrecedence(BinaryOpNode *other) {
+  return getOpPrecedence() > other->getOpPrecedence();
+}
+
+bool BinaryOpNode::samePrecedence(BinaryOpNode *other) {
+  return getOpPrecedence() == other->getOpPrecedence();
+}
+
+unsigned BinaryOpNode::getOpPrecedence() {
+  switch (this->op) {
+    case Add:
+    case Sub:
+      return 0;
+    case Mul:
+    case Div:
+      return 1;
+    default:
+      unknownOperatorError();
+  }
+  return 0;
+}
+
+void BinaryOpNode::unknownOperatorError() {
+  std::cerr << "BinaryOpNode has unknown operator '" << this->op << "'" << std::endl;
+  exit(1);
+}
+
 std::string IdentifierNode::codeGen() {
+  return this->identifier;
+}
+
+std::string IdentifierNode::toString() {
   return this->identifier;
 }
 
@@ -49,6 +119,10 @@ std::string NumberNode::codeGen() {
   std::ostringstream ss;
   ss << this->number;
   return std::string(ss.str());
+}
+
+std::string NumberNode::toString() {
+  return this->codeGen();
 }
 
 float NumberNode::getNumber() {
